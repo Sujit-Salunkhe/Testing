1d2c779d1475a0341fea7fcb81db478c06e7fb2c
Sujit Salunkhe <sujitsalunkhe233@gmail.com>
2026-01-22 13:31:26 +0530

    testing apache format

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 // import DataGrid from './components/DataGrid'
-import DataTable from './components/DataTable'
-import DataTableComponet from './components/DataTableComponet'
+// import DataTable from './components/DataTable'
+// import DataTableComponet from './components/DataTableComponet'
 // import Sonnar from './components/Sonnar'
-import DataGrid from './components/DataGridTest'
-import DataGrid3 from './components/DataGridTest2'
-import DataGrid4 from './components/DataGridTest3'
+// import DataGrid from './components/DataGridTest'
+// import DataGrid3 from './components/DataGridTest2'
+// import DataGrid4 from './components/DataGridTest3'
+// import DataGrid4 from './components/DataGrid4'
+import ArrowTestComponent from './components/DataGridTest3'
 function App() {
   // const [count, setCount] = useState(0)
 
@@ -16,11 +18,13 @@ function App() {
     <>
     <h1>heading</h1>
     {/* <DataGrid/> */}
-    <DataGrid3/>
+    {/* <DataGrid3/> */}
+    <ArrowTestComponent/>
     {/* <DataGrid4/> */}
     {/* <DataTable/> */}
     {/* <Sonnar/> */}
     {/* <DataTableComponet/> */}
+    {/* <DataGrid4/> */}
     </>
   )
 }
